ba23c661805d9ca45c5808795538381899c56655
carneirofc <claudiofcarneiro@hotmail.com>
2021-01-06 10:38:43 -0300

    Removed port from request

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,3 +1,3 @@
 export default {
-  MAILPY_API_URL: `${window.location.protocol}//${window.location.hostname}:1337/mailpy/api`,
+  MAILPY_API_URL: `${window.location.protocol}//${window.location.hostname}/mailpy/api`,
 };
